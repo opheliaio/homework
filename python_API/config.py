40c1ac578cbc9c5538e11a4b20916fc05f3ebedc
opheliaio <ophelia_io@hotmail.com>
2018-06-15 00:02:12 -0700

    Homework upload

---
+++ python_API/config.py
@@ -0,0 +1,1 @@
+api_key="1a119b60aa2d2168135e19bbc9b07aa9"
---
+++ social_analytic/config.py
@@ -0,0 +1,6 @@
+consumer_key = "KfQREHVj3Ftv6bnVBdtOEqLD4"
+consumer_secret = "mtj902vERxCD1fVLHOK8TBohs0lPXDVuhbpgTdtgnrXDrBi94n"
+access_token = "69068583-JtNmpo0COvtmQbkQkuE1AojxjNXGgw0NQt2BeAoh9" 
+access_token_secret = "OAgU3ZVhvVhqY27lpbmnBkQPDit5WGalEFmn322dPQCMP"
+
+
